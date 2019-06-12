1ef538e3906245f0711fdf84961238db9b383e11
patheticGeek <shivamnagi14@gmail.com>
2019-06-12 12:24:10 +0530

    getWebSeries added, getMovie renamed

--- index.js
+++ index.js
@@ -37,11 +37,10 @@ async function getMovie(link) {
 
       return { error: false, movie: { title, runtime, ratings, poster, summary, genre, creators, stars } };
     });
-    if (dev) {
-      console.log(movie);
-    }
+
     await page.close();
     await browser.close();
+
     return movie;
   } catch (err) {
     console.log(err);
@@ -65,14 +64,29 @@ async function getWebSeries(link) {
       if (!movieWrapper) {
         return { error: true, message: 'No such page' };
       }
+      var genreWrapper = movieWrapper.querySelectorAll('div.subtext > a:not(:last-child)');
+      var creatorsWrapper = movieWrapper.querySelectorAll('div.credit_summary_item')[0];
+      var starsWrapper = movieWrapper.querySelectorAll('div.credit_summary_item')[1];
 
-      return { error: false };
+      starsWrapper.querySelectorAll('a:not(:last-child)').forEach(anchor => stars.push(anchor.innerText));
+      creatorsWrapper.querySelectorAll('a').forEach(async anchor => creators.push(anchor.innerText));
+      genreWrapper.forEach(async anchor => genre.push(anchor.innerText));
+
+      title = movieWrapper.querySelector('div.title_wrapper > h1').innerText;
+      runtime = movieWrapper.querySelector('time').innerText;
+      poster = movieWrapper.querySelector('div.slate_wrapper > div.poster > a > img').src;
+      summary = movieWrapper.querySelector('div.plot_summary > div.summary_text').innerText;
+      ratings = movieWrapper.querySelector('div.ratingValue').innerText;
+
+      return {
+        error: false,
+        webseries: { title, runtime, ratings, poster, summary, genre, creators, stars }
+      };
     });
-    if (dev) {
-      console.log(movie);
-    }
+
     await page.close();
     await browser.close();
+
     return movie;
   } catch (err) {
     console.log(err);
@@ -91,14 +105,23 @@ server.get('/', (req, res) => {
 
 server.get('/getMovie', async (req, res) => {
   let link = req.query.link;
+
   if (link === '' || !link) {
-    if (dev) {
-      console.log('link: empty');
-    }
     res.send({ error: true, errorMessage: 'Link cannot be empty' });
   } else {
-    console.log('link: ', link);
+    console.log('Movie link: ', link);
     res.send(await getMovie(link));
+  }
+});
+
+server.get('/getWebSeries', async (req, res) => {
+  let link = req.query.link;
+
+  if (link === '' || !link) {
+    res.send({ error: true, errorMessage: 'Link cannot be empty' });
+  } else {
+    console.log('WebSeries link: ', link);
+    res.send(await getWebSeries(link));
   }
 });
 
